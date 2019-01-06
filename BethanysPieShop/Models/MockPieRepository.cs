d2b2fd0220ed8b3881df1c73093f3a16dc52e8ee
Rakesh Baruah <rbaruah@mac.com>
2019-01-05 19:17:40 -0600

    Created primary domain class for the Model as well as the interface for its Repository API

---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+
+        public MockPieRepository()
+        {
+            if (_pies == null)
+            {
+                InitializePies();
+            }
+        }
+
+        private void InitializePies()
+        {
+            // This method will fill the _pies List with Pie objects...from where?
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
