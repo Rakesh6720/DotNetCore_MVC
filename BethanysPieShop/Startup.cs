dc390aead6ac25a6f35d625ba4b0ad609d8f3fe8
Rakesh Baruah <rbaruah@mac.com>
2019-01-05 22:26:28 -0600

    Configure Services setup and connection string added to Startup class.

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,6 +6,8 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BethanysPieShop
@@ -14,9 +16,22 @@ namespace BethanysPieShop
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        // This is done in the Program.Main() method 
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();  // Original Repository class added to services container for Dependency Injection           
+            // Setup EF Core
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); // SqlServer needs connectionString
+
+            services.AddTransient<IPieRepository, PieRepository>();  // Original Repository class added to services container for Dependency Injection           
             services.AddMvc();
             
         }
