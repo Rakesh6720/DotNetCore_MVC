544b56192f28520b1ac4b2af042fb5d2ff0987b8
Rakesh Baruah <rbaruah@mac.com>
2019-01-05 20:35:14 -0600

    Added home view model as wel as view models folder to project.  index action method now returns viewModel from the homeController, which holds a Title and List of Pies properties.

--- BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -23,13 +24,17 @@ namespace BethanysPieShop.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.Title = "Pie Overview";
-
             var pies = _pieRepository
                         .GetAllPies()
                         .OrderBy(p => p.Name);
 
-            return View(pies);
+            var viewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }
