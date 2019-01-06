cbbcb8c6784bbd70ec914f9a9c496f5e2b145910
Rakesh Baruah <rbaruah@mac.com>
2019-01-05 19:55:02 -0600

    About to setup the View...

--- BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +11,15 @@ namespace BethanysPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            // ConfigureServices() creates a MockPieRepository for each IPieRepository
+            // ... so pieRepository will be a MockPieRepository
+            _pieRepository = pieRepository;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
